a66fae84bf72e94040b5bd1e32e3859d55fa374d
pseeth <prem@u.northwestern.edu>
2019-02-24 23:43:54 -0600

    adjusting test cases for all datasets

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,28 +1,41 @@
 import numpy as np 
 from code import run
+import os
+
+datasets = [
+    os.path.join('data', x) 
+    for x in os.listdir('data') 
+    if '.csv' in x
+]
 
 def test_experiment_run_decision_tree():
-    data_path = 'data/candy-data.csv'
-    learner_type = 'decision_tree'
-    confusion_matrix, accuracy, precision, recall, f1_measure = (
-        run(data_path, learner_type, .9)
-    )
-    assert (accuracy > .8)
+    accuracies = {}
+    for data_path in datasets:
+        learner_type = 'decision_tree'
+        confusion_matrix, accuracy, precision, recall, f1_measure = (
+            run(data_path, learner_type, .9)
+        )
+        accuracies[data_path] = accuracy
+    assert (accuracies['data/ivy-league.csv'] > .8)
     
 
 def test_experiment_run_prior_probability():
-    data_path = 'data/candy-data.csv'
-    learner_type = 'prior_probability'
-    confusion_matrix, accuracy, precision, recall, f1_measure = (
-        run(data_path, learner_type, .9)
-    )
-    assert (accuracy > .2)
+    accuracies = {}
+    for data_path in datasets:
+        learner_type = 'prior_probability'
+        confusion_matrix, accuracy, precision, recall, f1_measure = (
+            run(data_path, learner_type, .9)
+        )
+        accuracies[data_path] = accuracy
+    assert (accuracies['data/ivy-league.csv'] > .2)
 
 def test_experiment_run_and_compare():
-    data_path = 'data/candy-data.csv'
-    learner_types = ['prior_probability', 'decision_tree']
-    accuracies = {}
-    for learner_type in learner_types:
-        accuracies[learner_type] = run(data_path, learner_type, .9)[1]
-
-    assert (accuracies['decision_tree'] > accuracies['prior_probability'])
+    for data_path in datasets:
+        accuracies = {}
+        learner_types = ['prior_probability', 'decision_tree']
+        for learner_type in learner_types:
+            accuracies[learner_type] = run(data_path, learner_type, .9)[1]
+        if 'candy' in data_path or 'ivy' in data_path:
+            assert (
+                accuracies['decision_tree'] > accuracies['prior_probability']
+            )
