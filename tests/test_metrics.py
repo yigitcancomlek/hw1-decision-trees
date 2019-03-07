96e8b58772771ba347e46fe817600a6a2819f14a
pseeth <prem@u.northwestern.edu>
2019-03-07 12:46:13 -0600

    changing to np.allclose

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -8,7 +8,7 @@ def test_accuracy():
     y_true, y_pred = make_fake_data()
     _actual = accuracy_score(y_true, y_pred)
     _est = accuracy(y_true, y_pred)
-    assert _actual == _est
+    assert np.allclose(_actual, _est)
 
 def test_f1_measure():
     from sklearn.metrics import f1_score
@@ -17,7 +17,7 @@ def test_f1_measure():
     y_true, y_pred = make_fake_data()
     _actual = f1_score(y_true, y_pred)
     _est = f1_measure(y_true, y_pred)
-    assert _actual == _est
+    assert np.allclose(_actual, _est)
 
 def test_precision_and_recall():
     from sklearn.metrics import precision_score, recall_score
@@ -26,7 +26,7 @@ def test_precision_and_recall():
     y_true, y_pred = make_fake_data()
     _actual = [precision_score(y_true, y_pred), recall_score(y_true, y_pred)]
     _est = precision_and_recall(y_true, y_pred)
-    assert _actual == _est
+    assert np.allclose(_actual, _est)
 
 def test_confusion_matrix():
     from sklearn.metrics import confusion_matrix as ref_confusion_matrix
@@ -35,4 +35,4 @@ def test_confusion_matrix():
     y_true, y_pred = make_fake_data()
     _actual = ref_confusion_matrix(y_true, y_pred)
     _est = est_confusion_matrix(y_true, y_pred)
-    assert _actual == _est
+    assert np.allclose(_actual, _est)
