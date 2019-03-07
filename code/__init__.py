2f325dbae3a35dd0d87f7ca54fbe7b75becdbb74
pseeth <prem@u.northwestern.edu>
2019-03-07 13:49:03 -0600

    accuracy to import

--- code/__init__.py
+++ code/__init__.py
@@ -1,4 +1,4 @@
-from .metrics import f1_measure, precision_and_recall, confusion_matrix
+from .metrics import f1_measure, precision_and_recall, confusion_matrix, accuracy
 from .data import load_data, train_test_split
 from .decision_tree import DecisionTree, information_gain
 from .prior_probability import PriorProbability
