f60a014f4c5d3014773c07a67f85535ccccf99ef
Max Morrison <maxrmorrison@gmail.com>
2019-04-03 17:56:26 -0500

    Fixed visualization for nonetype tree values

--- code/decision_tree.py
+++ code/decision_tree.py
@@ -3,24 +3,24 @@ import numpy as np
 class Tree():
     def __init__(self, value=None, attribute_name="root", attribute_index=None, branches=None):
         """
-        This class implements a tree structure with multiple branches at each node. 
+        This class implements a tree structure with multiple branches at each node.
         If self.branches is an empty list, this is a leaf node and what is contained in
         self.value is the predicted class.
 
         The defaults for this are for a root node in the tree.
 
         Arguments:
-            branches (list): List of Tree classes. Used to traverse the tree. In a 
+            branches (list): List of Tree classes. Used to traverse the tree. In a
                 binary decision tree, the length of this list is either 2 (for left and
                 right branches) or 0 (at a leaf node).
             attribute_name (str): Contains name of attribute that the tree splits the data
                 on. Used for visualization (see `DecisionTree.visualize`).
-            attribute_index (float): Contains the  index of the feature vector for the 
+            attribute_index (float): Contains the  index of the feature vector for the
                 given attribute. Should match with self.attribute_name.
             value (number): Contains the value that data should be compared to along the
                 given attribute.
         """
-        self.branches = [] if branches is None else branches 
+        self.branches = [] if branches is None else branches
         self.attribute_name = attribute_name
         self.attribute_index = attribute_index
         self.value = value
@@ -34,21 +34,21 @@ class DecisionTree():
         categorical attributes. Use the ID3 algorithm for implementing the Decision
         Tree: https://en.wikipedia.org/wiki/ID3_algorithm
 
-        A decision tree is a machine learning model that fits data with a tree 
-        structure. Each branching point along the tree marks a decision (e.g. 
+        A decision tree is a machine learning model that fits data with a tree
+        structure. Each branching point along the tree marks a decision (e.g.
         today is sunny or today is not sunny). Data is filtered by the value of
         each attribute to the next level of the tree. At the next level, the process
         starts again with the remaining attributes, recursing on the filtered data.
 
         Which attributes to split on at each point in the tree are decided by the
-        information gain of a specific attribute. 
+        information gain of a specific attribute.
 
         Here, you will implement a binary decision tree that uses the ID3 algorithm.
-        Your decision tree will be contained in `self.tree`, which consists of 
+        Your decision tree will be contained in `self.tree`, which consists of
         nested Tree classes (see above).
 
         Args:
-            attribute_names (list): list of strings containing the attribute names for 
+            attribute_names (list): list of strings containing the attribute names for
                 each feature (e.g. chocolatey, good_grades, etc.)
 
         """
@@ -69,11 +69,11 @@ class DecisionTree():
         Args:
             features (np.array): numpy array of size NxF containing features, where N is
                 number of examples and F is number of features.
-            targets (np.array): numpy array containing class labels for each of the N 
+            targets (np.array): numpy array containing class labels for each of the N
                 examples.
         """
         self._check_input(features)
-        
+
 
         raise NotImplementedError()
 
@@ -95,12 +95,13 @@ class DecisionTree():
         Helper function for visualize a decision tree at a given level of recursion.
         """
         tab_level = "  " * level
-        print("%d: %s%s == %f" % (level, tab_level, tree.attribute_name, tree.value))
+        val = tree.value if tree.value is not None else 0
+        print("%d: %s%s == %f" % (level, tab_level, tree.attribute_name, val))
 
     def visualize(self, branch=None, level=0):
         """
         Visualization of a decision tree. Implemented for you to check your work and to
-        use as an example of how to use the given classes to implement your decision 
+        use as an example of how to use the given classes to implement your decision
         tree.
         """
         if not branch:
@@ -114,60 +115,60 @@ def information_gain(features, attribute_index, targets):
     """
     TODO: Implement me!
 
-    Information gain is how a decision tree makes decisions on how to create 
+    Information gain is how a decision tree makes decisions on how to create
     split points in the tree. Information gain is measured in terms of entropy.
-    The goal of a decision tree is to decrease entropy at each split point as much as 
+    The goal of a decision tree is to decrease entropy at each split point as much as
     possible. This function should work perfectly or your decision tree will not work
     properly.
 
     Information gain is a central concept in many machine learning algorithms. In
     decision trees, it captures how effective splitting the tree on a specific attribute
-    will be for the goal of classifying the training data correctly. Consider 
+    will be for the goal of classifying the training data correctly. Consider
     data points S and an attribute A. S is split into two data points given binary A:
 
         S(A == 0) and S(A == 1)
 
     Together, the two subsets make up S. If A was an attribute perfectly correlated with
-    the class of each data point in S, then all points in a given subset will have the 
+    the class of each data point in S, then all points in a given subset will have the
     same class. Clearly, in this case, we want something that captures that A is a good
     attribute to use in the decision tree. This something is information gain. Formally:
 
         IG(S,A) = H(S) - H(S|A)
 
     where H is information entropy. Recall that entropy captures how orderly or chaotic
-    a system is. A system that is very chaotic will evenly distribute probabilities to 
+    a system is. A system that is very chaotic will evenly distribute probabilities to
     all outcomes (e.g. 50% chance of class 0, 50% chance of class 1). Machine learning
-    algorithms work to decrease entropy, as that is the only way to make predictions 
+    algorithms work to decrease entropy, as that is the only way to make predictions
     that are accurate on testing data. Formally, H is defined as:
 
         H(S) = sum_{c in (classes in S)} -p(c) * log_2 p(c)
 
     To elaborate: for each class in S, you compute its prior probability p(c):
 
-        (# of elements of class c in S) / (total # of elements in S) 
+        (# of elements of class c in S) / (total # of elements in S)
 
-    Then you compute the term for this class: 
+    Then you compute the term for this class:
 
         -p(c) * log_2 p(c)
 
-    Then compute the sum across all classes. The final number is the entropy. To gain 
-    more intution about entropy, consider the following - what does H(S) = 0 tell you 
-    about S? 
+    Then compute the sum across all classes. The final number is the entropy. To gain
+    more intution about entropy, consider the following - what does H(S) = 0 tell you
+    about S?
 
-    Information gain is an extension of entropy. The equation for information gain 
+    Information gain is an extension of entropy. The equation for information gain
     involves comparing the entropy of the set and the entropy of the set when conditioned
-    on selecting for a single attribute (e.g. S(A == 0)). 
+    on selecting for a single attribute (e.g. S(A == 0)).
 
     For more details: https://en.wikipedia.org/wiki/ID3_algorithm#The_ID3_metrics
 
     Args:
         features (np.array): numpy array containing features for each example.
-        attribute_index (int): which column of features to take when computing the 
+        attribute_index (int): which column of features to take when computing the
             information gain
         targets (np.array): numpy array containing labels corresponding to each example.
 
     Output:
-        information_gain (float): information gain if the features were split on the 
+        information_gain (float): information gain if the features were split on the
             attribute_index.
     """
 
@@ -181,7 +182,7 @@ if __name__ == '__main__':
         attribute_name = attribute_names[0]
         if not decision_tree.tree:
             decision_tree.tree = Tree(
-                attribute_name=attribute_name, 
+                attribute_name=attribute_name,
                 attribute_index=decision_tree.attribute_names.index(attribute_name),
                 value=0,
                 branches=[]
